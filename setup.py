e0631fd212580bf00fc3e00e74b0071e89b4de23
Martin Mrose <github@penthranet.de>
2015-07-14 15:42:03 +0200

    Updated requirements notation

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 from setuptools import setup
 
 
+def get_requirements(requirements_file):
+    """
+    Returns a list of package requirements read from the given file.
+    :param requirements_file: the file to read
+    :return: list of package requirements
+    """
+    with open(requirements_file, 'r') as f:
+        return [line for line in f.read().splitlines() if not line.startswith('#')]
+
+
 setup(
     name='django-ldap-server',
     description='share django users and groups with other services via ldap',
@@ -12,9 +22,6 @@ setup(
     url='https://github.com/ponyriders/django-ldap-server',
     packages=['django_ldap_server'],
     long_description=open('README.rst').read(),
-    install_requires = [
-        'gevent>=1.1a1',
-        'Django==1.8',
-        'pyasn1-modules>=0.1.8'
-    ],
+    install_requires = get_requirements('requirements.txt'),
+    tests_require=get_requirements('requirements_test.txt'),
 )
