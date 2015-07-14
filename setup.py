f554834fdbb4d6abf03194d218761060c87ced0a
Martin Mrose <github@penthranet.de>
2015-07-14 15:02:13 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,14 @@ setup(
     description='share django users and groups with other services via ldap',
     version='0.1.0',
     author='Lars Kreisz & Martin Mrose',
-    author_email='der.kraiz@gmail.com',
+    author_email='django-ldap-server@penthranet.de',
     license='MIT',
     url='https://github.com/ponyriders/django-ldap-server',
     packages=['django_ldap_server'],
     long_description=open('README.rst').read(),
     install_requires = [
-        'gevent==dev',
-        'django>=1.4',
-        'pyasn1-modules==0.0.5'
+        'gevent>=1.1a1',
+        'django>=1.8',
+        'pyasn1-modules>=0.1.8'
     ],
-    dependency_links= [
-        'https://github.com/surfly/gevent/tarball/master#egg=gevent'
-    ]
 )
